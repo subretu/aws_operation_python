cfba99cba837447bae3e2ff3bac51f725cc285e2
miyahara <50367145+subretu@users.noreply.github.com>
2022-10-19 23:11:39 +0900

    データ取得数制限対策

--- s3_boto3.py
+++ s3_boto3.py
@@ -1,11 +1,23 @@
 import boto3
 
-# 対象バケットのオブジェクト一覧を表示
+
 s3_client = boto3.client("s3")
 bucket_name = "exampleread00000000"
 folder_name = "test1"
+
 response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
-for obj in response["Contents"]:
-    if obj["Size"] > 0:
-        print(obj)
-        print(obj["Key"])
+while True:
+    # バケット内のファイルを表示
+    for obj in response["Contents"]:
+        # フォルダはsizeが0となるため除外
+        if obj["Size"] > 0:
+            print(obj["Key"])
+
+    # NextContinuationTokenが存在する場合は次のデータ取得
+    if "NextContinuationToken" in response:
+        token = response["NextContinuationToken"]
+        response = s3_client.list_objects_v2(
+            Bucket=bucket_name, Prefix=folder_name, ContinuationToken=token
+        )
+    else:
+        break
