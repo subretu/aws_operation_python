b97f9f87f6feff25d594729a1c29e99c60a78838
miyahara <50367145+subretu@users.noreply.github.com>
2022-10-19 22:48:02 +0900

    フォルダ指定

--- s3_boto3.py
+++ s3_boto3.py
@@ -1,9 +1,11 @@
 import boto3
 
 # 対象バケットのオブジェクト一覧を表示
-s3_client = boto3.client('s3')
-bucket_name = "mailbody00001111"
-response = s3_client.list_objects_v2(Bucket=bucket_name)
-for obj in response['Contents']:
-    print(obj)
-    print(obj['Key'])
+s3_client = boto3.client("s3")
+bucket_name = "exampleread00000000"
+folder_name = "test1"
+response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
+for obj in response["Contents"]:
+    if obj["Size"] > 0:
+        print(obj)
+        print(obj["Key"])
