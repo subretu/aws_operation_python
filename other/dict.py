0b5d87a64c43bef522c170a67386c142eba54242
miyahara <50367145+subretu@users.noreply.github.com>
2022-12-27 23:59:06 +0900

    refactor

--- other/dict.py
+++ other/dict.py
@@ -15,42 +15,33 @@ dict_alarm = {}
 dict_time = {}
 
 for index, row in df.iterrows():
-    if len(row["alarm"].split(",")) > 1:
-        for alarm in row["alarm"].split(","):
-            if (MASTER.get(alarm) not in dict_alarm.keys()) and (
-                MASTER.get(alarm) not in dict_time.keys()
-            ):
-                dict_alarm[MASTER.get(alarm)] = {}
-                dict_time[MASTER.get(alarm)] = {}
+      for alarm in row["alarm"].split(","):
+          if (MASTER.get(alarm) not in dict_alarm.keys()) and (
+              MASTER.get(alarm) not in dict_time.keys()
+          ):
+              # 最初に各keyを定義し値を設定していく
+              dict_alarm[MASTER.get(alarm)] = {}
+              dict_time[MASTER.get(alarm)] = {}
 
-                dict_alarm[MASTER.get(alarm)] = set()
-                dict_time[MASTER.get(alarm)] = 0
+              dict_alarm[MASTER.get(alarm)] = set()
+              dict_time[MASTER.get(alarm)] = 0
 
-                dict_alarm[MASTER.get(alarm)].add(alarm)
-                dict_time[MASTER.get(alarm)] = row["time_msec"]
-            else:
-                dict_alarm[MASTER.get(alarm)].add(alarm)
-                if MASTER.get(alarm) not in dict_time.keys():
-                    dict_time[MASTER.get(alarm)] = (
-                        dict_time[MASTER.get(alarm)] + row["time_msec"]
-                    )
-    else:
-        if (MASTER.get(row["alarm"]) not in dict_alarm.keys()) and (
-            MASTER.get(row["alarm"]) not in dict_time.keys()
-        ):
-            dict_alarm[MASTER.get(row["alarm"])] = {}
-            dict_time[MASTER.get(row["alarm"])] = {}
-
-            dict_alarm[MASTER.get(row["alarm"])] = set()
-            dict_time[MASTER.get(row["alarm"])] = 0
-
-            dict_alarm[MASTER.get(row["alarm"])].add(row["alarm"])
-            dict_time[MASTER.get(row["alarm"])] = row["time_msec"]
-        else:
+              dict_alarm[MASTER.get(alarm)].add(alarm)
+              dict_time[MASTER.get(alarm)] = row["time_msec"]
+          # alarmが単一の場合
+          elif len(row["alarm"].split(",")) == 1:
             dict_alarm[MASTER.get(row["alarm"])].add(row["alarm"])
             dict_time[MASTER.get(row["alarm"])] = (
                 dict_time[MASTER.get(row["alarm"])] + row["time_msec"]
             )
+          else:
+              # keyに重複がなければ値を変更する
+              dict_alarm[MASTER.get(alarm)].add(alarm)
+              if MASTER.get(alarm) not in dict_time.keys():
+                  dict_time[MASTER.get(alarm)] = (
+                      dict_time[MASTER.get(alarm)] + row["time_msec"]
+                  )
+
 
 # 値の降順でソートし、dictを再作成
 dict_time2 = dict(sorted(dict_time.items(), key=lambda x: x[1], reverse=True))
