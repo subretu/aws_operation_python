4329c08c8666e5f360db5107f0ed581dc8145052
miyahara <50367145+subretu@users.noreply.github.com>
2023-03-31 22:31:31 +0900

    add

---
+++ other/selected_line_renumbering.py
@@ -0,0 +1,66 @@
+# 選択されたID
+tareget_id = ["c", "e", "g"]
+tareget_id_count = len(tareget_id)
+
+# 指定された開始の順番
+start_num = 3
+
+# 対象IDの元の番号を取得（別途関数を用意）
+tareget_id_num = [5, 7, 9]
+
+# ここでtaegetが該当する行のupdate文を作成（TBD）
+for id in tareget_id:
+    pass
+
+# 残りの行更新用のupdate文
+if start_num > 1:
+    sql_1st = f"""
+    update sample
+      set
+        num = num + {tareget_id_count}
+    where
+      {start_num} < num and num < {tareget_id_num[0]}
+    ;
+    """
+
+    print(sql_1st)
+
+    # 残りの行を更新
+    for i in range(1, tareget_id_count):
+        keisuu = tareget_id_count - i
+        sql_2st = f"""
+        update sample
+          set
+            num = num + ({keisuu})
+        where
+          {tareget_id_num[i-1]} < num and num < {tareget_id_num[i]}
+        ;
+        """
+
+        print(sql_2st)
+# 指定の開始順番が1の場合
+else:
+    sql_1st = f"""
+    update sample
+      set
+        num = num + {tareget_id_count}
+    where
+      num < {tareget_id_num[0]}
+    ;
+    """
+
+    print(sql_1st)
+
+    # 残りの行を更新
+    for i in range(1, tareget_id_count):
+        keisuu = tareget_id_count - i
+        sql_2st = f"""
+        update sample
+          set
+            num = num+({keisuu})
+          where
+            {tareget_id_num[i-1]} < num and num < {tareget_id_num[i]}
+        ;
+        """
+
+        print(sql_2st)
