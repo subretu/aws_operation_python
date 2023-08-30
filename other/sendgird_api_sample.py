d620f2d543c9dbda243151cd750de7155c353cad
miyahara <50367145+subretu@users.noreply.github.com>
2023-08-30 22:57:26 +0900

    try sendgrid api

---
+++ other/sendgird_api_sample.py
@@ -0,0 +1,107 @@
+from io import StringIO, BytesIO
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from base64 import b64encode
+from csv import DictWriter
+from sendgrid.helpers.mail import (
+    Mail,
+    Attachment,
+    FileContent,
+    FileName,
+    FileType,
+    Disposition,
+)
+from sendgrid import SendGridAPIClient
+from zipfile import ZIP_DEFLATED, ZipFile
+
+
+def main():
+    message = create_message()
+
+    sendgird_api_key = "samplekeyhogehoge"
+    send_mail(message, sendgird_api_key)
+
+
+def create_message(mailAddress):
+    htmlText = """
+    {name}様<br><br>
+
+    メールを送ります。
+    """
+    name = " hogehoge"
+    textData = htmlText.format(name=name)
+
+    message = Mail(
+        from_email="no-reply@example.com",
+        to_emails=mailAddress,
+        subject="【TEST】テスト",
+        html_content=textData,
+    )
+
+    param = {
+        "port": "54321",
+        "user": "postgres",
+        "password": "postgres",
+        "host": "localhost",
+        "dbname": "postgres",
+    }
+
+    query = """
+    select
+        id
+        ,name
+    from
+        hoge
+    ;
+    """
+
+    columns_csv = ["ID", "名前"]
+
+    with psycopg2.connect(**param) as conn:
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+        cursor.execute(query)
+        data = cursor.fetchall()
+
+        sio = StringIO()
+        writer = DictWriter(
+            f=sio,
+            fieldnames=columns_csv,
+            lineterminator="\n",
+        )
+        writer.writeheader()
+        for i, record in enumerate(data):
+            writer.writerow({"ID": record["id"], "名前": record["name"]})
+        sss = sio.getvalue()
+
+        # インメモリでzipのバイナリを保持するためにBytesIOを使用
+        io = BytesIO()
+
+        # zipファイルを生成
+        with ZipFile(io, "w", compression=ZIP_DEFLATED, compresslevel=9) as zf:
+            zf.writestr("sample.csv", data=sss)
+        obj_bytes = io.getvalue()
+        raw = b64encode(obj_bytes).decode()
+
+        attached_file = Attachment(
+            FileContent(raw),
+            FileName("sample.zip"),
+            FileType("application/zip"),
+            Disposition("attachment"),
+        )
+
+        message.attachment = attached_file
+
+        return message
+
+
+def send_mail(message, sendgird_api_key):
+    try:
+        sg = SendGridAPIClient(sendgird_api_key)
+        response = sg.send(message)
+        print(response.status_code)
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
