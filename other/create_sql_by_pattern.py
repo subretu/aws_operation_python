bf98396ef6d03d002d5f0eb2a9929e9f190ab9e7
miyahara <50367145+subretu@users.noreply.github.com>
2024-07-20 23:39:27 +0900

    Add db connect

--- other/create_sql_by_pattern.py
+++ other/create_sql_by_pattern.py
@@ -1,3 +1,18 @@
+import psycopg2
+
+
+def get_db_connection():
+    connection = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="postgres",
+        database="postgres",
+        port=5432,
+    )
+
+    return connection
+
+
 def main(pattern: str):
     pattern_a = "opstime between '2022-03-01 10:00:00' and '2022-03-01 12:00:00'"
     pattern_b = "opstime = '2022-03-03 10:00:00'"
@@ -10,9 +25,17 @@ def main(pattern: str):
         pattern_var,
         "and value > 0;",
     ]
+
     sql = "\n".join(sql_parts)
-    print(sql)
+
+    with get_db_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(sql)
+            data = cursor.fetchall()
+            print(data)
+
+    conn.close()
 
 
 if __name__ == "__main__":
-    main(pattern="c")
+    main(pattern="a")
